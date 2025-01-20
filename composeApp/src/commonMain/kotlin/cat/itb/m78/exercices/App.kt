212c7ace2ecbaa5d1080d9586f57b0c16b0f5e18
aleixtuneu <aleix.tuneu.7e7@itb.cat>
2025-01-20 12:58:22 +0100

    Exercici SayHelloName

--- composeApp/src/commonMain/kotlin/cat/itb/m78/exercices/App.kt
+++ composeApp/src/commonMain/kotlin/cat/itb/m78/exercices/App.kt
@@ -6,12 +6,34 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import cat.itb.m78.exercices.theme.AppTheme
 import org.jetbrains.compose.reload.DevelopmentEntryPoint
 
 @Composable
 internal fun App() = AppTheme {
-    Box(Modifier.fillMaxSize()){
-        Text("Your app goes here!", Modifier.align(Alignment.Center))
+    Box(modifier = Modifier.fillMaxSize(),contentAlignment = Alignment.Center) {
+        var text by remember { mutableStateOf("") }
+        var showDialog by remember { mutableStateOf(false) }
+        Column(Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+            TextField(
+                value = text,
+                label = { Text("Name") },
+                onValueChange = { text = it }
+            )
+
+            Button(onClick = { showDialog = true }) {
+                Text("SayHello")
+            }
+
+            // Si showDialog és true, mostrar el AlertDialog
+            if (showDialog) {
+                AlertDialog(
+                    onDismissRequest = { showDialog = false },
+                    text = { Text("Hola $text") },
+                    confirmButton = {}
+                )
+            }
+        }
     }
 }
