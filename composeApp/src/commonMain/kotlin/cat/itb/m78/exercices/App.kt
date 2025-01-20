069af6b2d5c2e31e961bf3f96ffa7fc0f3d5745d
aleixtuneu <aleix.tuneu.7e7@itb.cat>
2025-01-20 13:04:30 +0100

    Exercici Resource

--- composeApp/src/commonMain/kotlin/cat/itb/m78/exercices/App.kt
+++ composeApp/src/commonMain/kotlin/cat/itb/m78/exercices/App.kt
@@ -1,17 +1,36 @@
 package cat.itb.m78.exercices
 
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import cat.itb.m78.exercices.theme.AppTheme
+import m78exercices.composeapp.generated.resources.Res
+import m78exercices.composeapp.generated.resources.generatedFace
 import org.jetbrains.compose.reload.DevelopmentEntryPoint
+import org.jetbrains.compose.resources.painterResource
 
 @Composable
-internal fun App() = AppTheme {
-    Box(Modifier.fillMaxSize()){
-        Text("Your app goes here!", Modifier.align(Alignment.Center))
+internal fun App() {
+    Box() {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Top
+        ){
+            Text("This is a resource string")
+            Image(
+                painter = painterResource(Res.drawable.generatedFace),
+                modifier = Modifier.size(100.dp),
+                contentDescription = null,
+            )
+        }
     }
 }
