0d14e54f66d374650f5cb282d56564c0d8b7e92b
aleixtuneu <aleix.tuneu.7e7@itb.cat>
2025-01-21 12:34:13 +0100

    Exercise Welcome

--- composeApp/src/commonMain/kotlin/cat/itb/m78/exercices/App.kt
+++ composeApp/src/commonMain/kotlin/cat/itb/m78/exercices/App.kt
@@ -1,17 +1,33 @@
 package cat.itb.m78.exercices
 
-
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.ui.Alignment
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.em
 import cat.itb.m78.exercices.theme.AppTheme
-import org.jetbrains.compose.reload.DevelopmentEntryPoint
+
 
 @Composable
 internal fun App() = AppTheme {
-    Box(Modifier.fillMaxSize()){
-        Text("Your app goes here!", Modifier.align(Alignment.Center))
+    Box(Modifier.fillMaxSize()) {
+        Column(modifier = Modifier.fillMaxSize(),
+               horizontalAlignment = Alignment.CenterHorizontally,
+               verticalArrangement = Arrangement.Center) {
+            Text("Welcome!",
+                fontSize = 2.em)
+            Text("Start learning now")
+            Button(onClick = { }) {
+                Text("Login",)
+            }
+            Button(onClick = { }) {
+                Text("Register")
+            }
+        }
     }
 }
