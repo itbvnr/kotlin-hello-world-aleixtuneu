274c7000fcbf97843b45ad4395b43213618e018a
aleixtuneu <aleix.tuneu.7e7@itb.cat>
2025-01-23 10:05:13 +0100

    Exercise Secret Number

--- composeApp/src/commonMain/kotlin/cat/itb/m78/exercices/App.kt
+++ composeApp/src/commonMain/kotlin/cat/itb/m78/exercices/App.kt
@@ -11,7 +11,45 @@ import org.jetbrains.compose.reload.DevelopmentEntryPoint
 
 @Composable
 internal fun App() = AppTheme {
-    Box(Modifier.fillMaxSize()){
-        Text("Your app goes here!", Modifier.align(Alignment.Center))
+    Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        var numberInput by remember { mutableStateOf("") }
+        var result by remember { mutableStateOf("Introdueix un número") }
+
+        val secretNumber = remember { (0..100).random() }
+        var intents by remember { mutableStateOf(0) };
+
+        println(secretNumber)
+
+        Column(Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+            TextField(
+                value = numberInput,
+                label = { Text("Endevina el número secret") },
+                onValueChange = { numberInput = it }
+            )
+
+            Button(onClick = {
+                if (numberInput.toIntOrNull() != null) {
+                    intents++
+                    if ((numberInput.toInt()) == secretNumber) {
+                        result = "Has endevinat el número secret!"
+                    } else {
+                        if (numberInput.toInt() < secretNumber) {
+                            result = "El número secret és mes gran"
+                        } else if (numberInput.toInt() > secretNumber) {
+                            result = "El número secret és més petit"
+                        }
+                    }
+                } else {
+                    intents++
+                    result = "Introdueix un valor vàlid"
+                }
+            }) {
+                Text("Validar")
+            }
+
+            Text("Intents $intents")
+
+            Text(result)
+        }
     }
 }
