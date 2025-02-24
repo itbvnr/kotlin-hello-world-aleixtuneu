73badd8134a094b6f76cf5e7761429869d489dd5
aleixtuneu <aleix.tuneu.7e7@itb.cat>
2025-02-24 11:32:41 +0100

    first commit

--- composeApp/src/commonMain/kotlin/cat/itb/m78/exercices/App.kt
+++ composeApp/src/commonMain/kotlin/cat/itb/m78/exercices/App.kt
@@ -11,7 +11,45 @@ import org.jetbrains.compose.reload.DevelopmentEntryPoint
 
 @Composable
 internal fun App() = AppTheme {
+    var productName by remember { mutableStateOf("") }
+    var productAmount by remember { mutableStateOf("") }
+    var productsList by remember { mutableStateOf(mutableMapOf<String, Int>()) }
+
     Box(Modifier.fillMaxSize()){
-        Text("Your app goes here!", Modifier.align(Alignment.Center))
+        // Afegir productes
+        Column(modifier = Modifier.fillMaxSize()) {
+            // Nom del producte
+            TextField(
+                value = productName,
+                label = { Text("Nom") },
+                onValueChange = { productName = it }
+            )
+
+            // Quantitat del producte
+            TextField(
+                value = productAmount,
+                label = { Text("Quantitat") },
+                onValueChange = { productAmount = it }
+            )
+
+            // Botó afegir
+            Button(onClick = {
+                val amount = productAmount.toIntOrNull()
+                if (productName.isNotBlank() && amount != null) {
+                    productsList = productsList.toMutableMap().apply {
+                        this[productName] = amount
+                    }
+                }
+            }) {
+                Text("Afegir")
+            }
+
+            // Mostrar llista de productes
+            Column {
+                productsList.forEach { (name, amount) ->
+                    Text("$name: $amount unitats")
+                }
+            }
+        }
     }
 }
