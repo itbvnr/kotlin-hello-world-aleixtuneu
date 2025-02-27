9ac2a1a1c8fb5d3873ecc5c522c250c32b12a61f
aleixtuneu <aleix.tuneu.7e7@itb.cat>
2025-02-27 10:20:18 +0100

    api commit

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -55,6 +55,7 @@ kotlin {
             implementation(libs.androidx.navigation.composee)
             implementation(libs.kotlinx.serialization.json)
             implementation(libs.kotlinx.datetime)
+            implementation("io.ktor:ktor-serialization-kotlinx-json:3.1.0")
         }
 
         commonTest.dependencies {
--- composeApp/src/commonMain/kotlin/cat/itb/m78/exercices/App.kt
+++ composeApp/src/commonMain/kotlin/cat/itb/m78/exercices/App.kt
@@ -1,17 +1,91 @@
 package cat.itb.m78.exercices
 
-
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.compose.viewModel
 import cat.itb.m78.exercices.theme.AppTheme
-import org.jetbrains.compose.reload.DevelopmentEntryPoint
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.get
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+
+/// 1. Model de dades
+@Serializable
+data class Joke(
+    val id: Int, val type: String, val setup: String, val punchline: String
+)
+
+// 2. Utilitzar ViewModel
+class JokesViewModel() : ViewModel() {
+    val joke = mutableStateOf<Joke?>(null)
+
+    // 3. Actualitzar l'objecte fent servir la api
+
+    // es llança en un fil a part
+    init {
+        viewModelScope.launch(Dispatchers.Default) {
+            joke.value = JokesApi.list().random()
+        }
+    }
+
+    fun getAJoke() {
+        viewModelScope.launch(Dispatchers.Default) {
+            joke.value = JokesApi.list().random()
+        }
+    }
+}
+
+// 4. Classe que fa servir la api
+object JokesApi {
+    // Atributs
+    val url = "https://api.sampleapis.com/jokes/goodJokes"
+    val client = HttpClient() {
+        install(ContentNegotiation) {
+            json(Json {
+                ignoreUnknownKeys = true
+            })
+        }
+    }
+
+    // Funcions
+    suspend fun list() = client.get(url).body<List<Joke>>()
+}
+
+@Composable
+fun JokesScreen() {
+    val viewModel = viewModel { JokesViewModel() }
+    val joke = viewModel.joke.value
+
+    Column(modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
+        if (joke != null) {
+            Text(joke.setup)
+            Text(joke.punchline)
+
+            Button(onClick= {viewModel.getAJoke()}) {
+                Text("Get a Joke")
+            }
+        } else {
+            CircularProgressIndicator()
+        }
+    }
+}
 
 @Composable
 internal fun App() = AppTheme {
-    Box(Modifier.fillMaxSize()){
-        Text("Your app goes here!", Modifier.align(Alignment.Center))
-    }
+    JokesScreen()
 }
