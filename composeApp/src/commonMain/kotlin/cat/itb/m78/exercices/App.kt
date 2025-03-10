c4c17a5987da5a7973a15e062d4028ab9e410d72
aleixtuneu <aleix.tuneu.7e7@itb.cat>
2025-03-10 12:43:08 +0100

    first commit

--- composeApp/src/commonMain/kotlin/cat/itb/m78/exercices/App.kt
+++ composeApp/src/commonMain/kotlin/cat/itb/m78/exercices/App.kt
@@ -1,30 +1,39 @@
 package cat.itb.m78.exercices
 
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 import cat.itb.m78.exercices.theme.AppTheme
 import com.russhwolf.settings.Settings
-import kotlinx.serialization.Serializable
 import com.russhwolf.settings.set
 
-// Iniciar Settings
-val settings: Settings = Settings()
+private const val COUNT_VIEW_KEY = "count"
+class CountViewViewModel : ViewModel(){
+    val settings: Settings = Settings()
+    val countViews = settings.getInt(COUNT_VIEW_KEY, 0)
+    init {
+        settings[COUNT_VIEW_KEY] = countViews+1
+    }
+}
+
+@Composable
+fun CountViewViewScreen(){
+    val viewModel = viewModel { CountViewViewModel() }
+    CountViewViewScreen(viewModel.countViews)
+}
+
+@Composable
+fun CountViewViewScreen(countViews: Int) {
+    Text("You have opened this app $countViews times")
+}
 
 @Composable
 internal fun App() = AppTheme {
-    // Obtenir el contador (si no existeix, se'n posa un valor per defecte de 0)
-    var openCount = settings.getIntOrNull("open_count") ?: 0
-
-    // Incrementar el contador cada vegada que s'obre la app
-    LaunchedEffect(key1 = Unit) {
-        openCount += 1
-        settings["open_count"] = openCount
-    }
-
-    Column {
-        Text(text = "Aquesta app s'ha obert $openCount vegades.")
-    }
+    CountViewViewScreen()
 }
+
