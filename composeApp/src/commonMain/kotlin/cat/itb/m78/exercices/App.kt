c407712d551e3200012099670736214a4ba2efa8
aleixtuneu <aleix.tuneu.7e7@itb.cat>
2025-03-10 12:10:51 +0100

    first commit

--- composeApp/src/commonMain/kotlin/cat/itb/m78/exercices/App.kt
+++ composeApp/src/commonMain/kotlin/cat/itb/m78/exercices/App.kt
@@ -1,17 +1,30 @@
 package cat.itb.m78.exercices
 
-
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import cat.itb.m78.exercices.theme.AppTheme
-import org.jetbrains.compose.reload.DevelopmentEntryPoint
+import com.russhwolf.settings.Settings
+import kotlinx.serialization.Serializable
+import com.russhwolf.settings.set
+
+// Iniciar Settings
+val settings: Settings = Settings()
 
 @Composable
 internal fun App() = AppTheme {
-    Box(Modifier.fillMaxSize()){
-        Text("Your app goes here!", Modifier.align(Alignment.Center))
+    // Obtenir el contador (si no existeix, se'n posa un valor per defecte de 0)
+    var openCount = settings.getIntOrNull("open_count") ?: 0
+
+    // Incrementar el contador cada vegada que s'obre la app
+    LaunchedEffect(key1 = Unit) {
+        openCount += 1
+        settings["open_count"] = openCount
+    }
+
+    Column {
+        Text(text = "Aquesta app s'ha obert $openCount vegades.")
     }
 }
