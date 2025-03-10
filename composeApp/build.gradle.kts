d62a618100498ddd8403f498985ac21aad8f5417
aleixtuneu <aleix.tuneu.7e7@itb.cat>
2025-03-10 12:38:04 +0100

    commit

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -55,6 +55,9 @@ kotlin {
             implementation(libs.androidx.navigation.composee)
             implementation(libs.kotlinx.serialization.json)
             implementation(libs.kotlinx.datetime)
+            implementation("io.ktor:ktor-serialization-kotlinx-json:3.1.0")
+            implementation("com.russhwolf:multiplatform-settings-no-arg:1.3.0")
+            implementation("com.russhwolf:multiplatform-settings-serialization:1.3.0")
         }
 
         commonTest.dependencies {
--- composeApp/src/commonMain/kotlin/cat/itb/m78/exercices/App.kt
+++ composeApp/src/commonMain/kotlin/cat/itb/m78/exercices/App.kt
@@ -1,17 +1,47 @@
 package cat.itb.m78.exercices
 
-
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.style.TextDirection
+import androidx.compose.ui.unit.dp
 import cat.itb.m78.exercices.theme.AppTheme
-import org.jetbrains.compose.reload.DevelopmentEntryPoint
+import com.russhwolf.settings.Settings
+import kotlinx.serialization.Serializable
+import com.russhwolf.settings.set
+
+// Iniciar Settings
+val settings: Settings = Settings()
 
 @Composable
 internal fun App() = AppTheme {
-    Box(Modifier.fillMaxSize()){
-        Text("Your app goes here!", Modifier.align(Alignment.Center))
+    val savedName = settings.getStringOrNull("user_name") ?: ""  // Recupera el nom des de Settings
+    val (name, setName) = remember { mutableStateOf(savedName) }
+    val (isNameSaved, setIsNameSaved) = remember { mutableStateOf(false) }  // Estat per controlar si el nom s'ha guardat
+
+    Column(modifier = Modifier.padding(16.dp)) {
+        // Mostrar el el nom si existeix
+        Text(text = "Hola $name!")
+
+
+        // Escriure el nom
+        TextField(
+            value = name,
+            onValueChange = { newValue -> setName(newValue) },  // Actualitza el nom
+            modifier = Modifier.padding(top = 8.dp)
+        )
+
+        // Botó per guardar el nom
+        Button(
+            onClick = {
+                settings["user_name"] = name // Guardar el nom a Settings
+                setIsNameSaved(true)  // Guardar el nom i actualitzar el text
+            },
+            modifier = Modifier.padding(top = 8.dp)
+        ) {
+            Text(text = "Save")
+        }
     }
 }
